b3296476659f46a45b86b6cd89de56b8eb677957
Hieu Phan <hieutrunglephan@gmail.com>
2019-10-30 22:41:35 +0700

    update cluster

--- readme.md
+++ readme.md
@@ -1,10 +1,6 @@
 # Private IPFS
 
-Ref: https://github.com/subhodi/ipfs-cluster-private
-
 ```bash
-$ go get github.com/Kubuxu/go-ipfs-swarm-key-gen/ipfs-swarm-key-gen # IPFS private secret shared key generation tool
-$ ipfs-swarm-key-gen > swarm.key # Generate secret key
 $ chmod +x start.sh
 $ ./start.sh
 ```
--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
-mkdir -p data/ipfsA data/ipfsB data/ipfsC
-tee data/ipfsA/swarm.key data/ipfsB/swarm.key data/ipfsC/swarm.key < swarm.key
-docker-compose up -d ipfsA ipfsB ipfsC
-echo "HOST_IP=192.168.1.17" > .env
-docker-compose up -d clusterA clusterB clusterC
+docker-compose up -d ipfs0 ipfs1 ipfs2
+docker-compose up -d cluster0 cluster1 cluster2
